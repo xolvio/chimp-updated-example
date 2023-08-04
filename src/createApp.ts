3851d4faa42a3086979cebf758c9b6c6d03ce33f
lgandecki <gozdak@gmail.com>
2023-08-04 17:03:07 +0200

    everything works with cors

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,19 +1,22 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import cors from "cors";
+import http from 'http';
+
 
 import { schema } from "~generated/graphql/schema";
 import { AppContext, appContext } from "~app/context";
 import { root } from "./root";
+import express from "express";
+import { json } from 'body-parser';
 
+import { expressMiddleware } from "@apollo/server/express4";
 // export const createApp = async () => {
 //   const app = express();
 //
 //   app.use([cookieParser()]);
 //
-//   const corsOptions = {
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   };
+
 //
 //   const apollo = new ApolloServer({
 //     schema,
@@ -21,18 +24,37 @@ import { root } from "./root";
 //   });
 //
 //   await apollo.start(); // Add this line
-//   apollo.applyMiddleware({ app, cors: corsOptions });
 //
 //   return app;
 // };
 
-const server = new ApolloServer<AppContext>({
+const apollo = new ApolloServer<AppContext>({
   schema,
 });
 
-const port = process.env.PORT || 4000;
+apollo.start().then(async () => {
+  const app = express();
 
-startStandaloneServer<AppContext>(server, {
-  context: appContext(root),
-  listen: { port: 4000 },
-}).then(() => {});
+
+  const httpServer = http.createServer(app);
+
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+  };
+
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(corsOptions),
+    json(),
+    // A named context function is required if you are not
+    // using ApolloServer<BaseContext>
+    expressMiddleware(apollo, {
+      context: appContext(root),
+    }),
+  );
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+
+});
